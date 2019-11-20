6ec3c548b9e852e2656ae58138da3a2638ab7520
zyuan2009 <29682067+zyuan2009@users.noreply.github.com>
2019-11-19 20:26:03 -0800

    Update math-app.js
    Add divide

--- math-app.js
+++ math-app.js
@@ -7,11 +7,11 @@ var yGap = 10;
 var numX = 6;
 var numY = 6;
 
-var firstMax = 99;
-var firstMin = 11;
+var firstMax = 999;
+var firstMin = 100;
 var secondMax = 99;
-var secondMin = 11;
-var operator = 'X';
+var secondMin = 2;
+var operator = '/';
 
 function generateRandom(min, max) {
     var init_val = Math.random() * (max - min + 1) + min;
@@ -41,16 +41,36 @@ function loadHTML() {
     var yTextStep = (yStep - 2 * yGap) / 4;
     for (var i = 0; i < numX; ++i) {
         for (var j = 0; j < numY; ++j) {
+            context.lineWidth = 2;
+            context.beginPath();
             context.rect(xStart + xStep * i + xGap, yStart + yStep * j + yGap, xStep - 2 * xGap, yStep - 2 * yGap);
             context.stroke();
-
-            var firstNumber = generateRandom(firstMin, firstMax);
-            var secondNumber = generateRandom(secondMin, secondMax);
-            context.fillText(firstNumber + "  ", xStart + xStep * i + xStep - xGap, yStart + yStep * j + yGap + yTextStep * 1);
-            context.fillText(operator + "      " + secondNumber + "  ", xStart + xStep * i + xStep - xGap, yStart + yStep * j + yGap + yTextStep * 2);
-            context.moveTo(xStart + xStep * i + xGap, yStart + yStep * j + yGap + yTextStep * 2 + 1);
-            context.lineTo(xStart + xStep * i + xStep - xGap, yStart + yStep * j + yGap + yTextStep * 2 + 1);
-            context.stroke();
+            if (operator != '/') {
+                var firstNumber = generateRandom(firstMin, firstMax);
+                var secondNumber = generateRandom(secondMin, secondMax);
+                context.fillText(firstNumber + "  ", xStart + xStep * i + xStep - xGap, yStart + yStep * j + yGap + yTextStep * 1);
+                context.fillText(operator + "      " + secondNumber + "  ", xStart + xStep * i + xStep - xGap, yStart + yStep * j + yGap + yTextStep * 2);
+                context.beginPath();
+                context.moveTo(xStart + xStep * i + xGap, yStart + yStep * j + yGap + yTextStep * 2 + 1);
+                context.lineTo(xStart + xStep * i + xStep - xGap, yStart + yStep * j + yGap + yTextStep * 2 + 1);
+                context.stroke();
+            } else {
+                var firstNumber = generateRandom(firstMin, firstMax);
+                var secondNumber = generateRandom(secondMin, secondMax);
+                firstNumber = Math.round(firstNumber / secondNumber) * secondNumber;
+                var secondMeasure = context.measureText(secondNumber.toString());
+                var firstMeasure = context.measureText(secondNumber.toString() + "    " + firstNumber.toString());
+                context.fillText(secondNumber.toString(), xStart + xStep * i + xGap * 2 + secondMeasure.width, yStart + yStep * j + yGap + yTextStep * 2);
+                context.fillText(firstNumber.toString(), xStart + xStep * i + xGap * 2 + firstMeasure.width, yStart + yStep * j + yGap + yTextStep * 2);
+                context.lineWidth = 5;
+                context.beginPath();
+                context.moveTo(xStart + xStep * i + xGap * 4 + secondMeasure.width, yStart + yStep * j + yGap + yTextStep - 1);
+                context.lineTo(xStart + xStep * i + xStep - xGap * 2, yStart + yStep * j + yGap + yTextStep - 1);
+                context.stroke();
+                context.beginPath();
+                context.arc(xStart + xStep * i + secondMeasure.width - xGap * 4, yStart + yStep * j + yGap + yTextStep - 1, xGap * 8, 0, Math.PI / 6);
+                context.stroke();
+            }
         }
     }
 }
